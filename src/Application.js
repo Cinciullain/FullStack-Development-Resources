0bd3a333d1d74666cf7ca13c2eea2a8fd5859b02
Cinciullain <casula.alessandro@outlook.com>
2021-03-17 16:33:54 +0100

    Add files via upload

--- src/Application.js
+++ src/Application.js
@@ -1,18 +1,71 @@
 import React, {Component} from 'react';
 
 class Application extends Component{
+    //Viene eseguito solo quando viene instanziata la pagina, una sola volta
+    constructor(props){
+        super(props);
 
+        this.state = {
+            count: 0,
+            overTen: false
+        }
+    }
+
+    //Gestisce il clock del bottone
+    handleCLick = () => { //Dichiarazione del metodo: "nomeMetodo = () => {} ""
+        console.log("Clicked"); //Punta sulla console
+        this.setState({count: this.state.count + 1})
+    }
+
+     /*
+    //È deprecato, trovare una alternativa
+    componentWillMount(props, state){}
+
+    componentDidMount(props, state){
+        console.log("Mounted with", props, state);
+    }
+
+    //Viene chiamato prima di essere montato, ricevendo le proprietà. È deprecato, trovare alternativa
+    componentWillReceiveProps(props){}
+
+    componentWillUpdate(props, state){
+        if(this.props.name !== props.name){}    
+    }
+    
+    */
+
+    //Modo più sicuro per gestire l'aggiornamento dei componenti
+    componentDidUpdate(props, state){
+        //Fa vedere alla console lo stato precedente e quello attuale, dopo essere aggiornato
+        //console.log("Updated from", state, "to", this.state);
+
+        if(this.state.count > 10 && this.state.count !== state.count && !this.state.overTen){
+            console.log("10 superato");
+            this.setState({overTen: true});
+        }
+    }
+    
     //Restituisce su browser HTML, testo o simili
     render(){
         let testo = "Test";
+        let {count} = this.state;
 
         return(
             <div>
-            <h1>{testo}, h1</h1>
-            <span>{testo}, span</span>
+                <h1>{testo}, h1</h1>
+                {(this.state.overTen) ?
+                    <h3>Record di 10 battuto</h3>
+                    : null
+                }
+                <span>{testo}, span</span>
+                <h1>Hai premuto il bottone {count} volte</h1>
+                <span>
+                    <button onClick = {() => this.handleCLick()}>Bottone</button>
+                </span>
             </div>
         );
     }
+
 }
 
 export default Application; //Serve per index.js
