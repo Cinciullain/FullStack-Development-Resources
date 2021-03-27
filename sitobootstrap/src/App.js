f9f96da0d81931d12570bbd5c0991fb434056bbe
Cinciullain <casula.alessandro@outlook.com>
2021-03-27 12:12:02 +0100

    Add files via upload

--- sitobootstrap/src/App.js
+++ sitobootstrap/src/App.js
@@ -1,11 +1,27 @@
+/* eslint-disable no-unused-vars */
 import PageWrapper from './components/PageWrapper';
 import Home from './components/Pages/Home';
 
+//Import di react router
+import{
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link
+} from "react-router-dom";
+
 function App() {
   return (
-	<PageWrapper>
-		<Home />
-	</PageWrapper>
+	<Router>
+		<PageWrapper>
+			{/*Path: della pagina, arbitrario
+			   Component: sostituisce la chiamata a Home.js nel wrapper di <Home/>*/}
+			<Route
+				path = "/" 
+				component = {Home}
+			/>
+		</PageWrapper>
+	</Router>
   );
 }
 
