b23714765828223ac38e00c967bca68b7503f372
Cinciullain <casula.alessandro@outlook.com>
2021-03-27 15:13:50 +0100

    Add files via upload

--- sitobootstrap/src/App.js
+++ sitobootstrap/src/App.js
@@ -1,6 +1,9 @@
 /* eslint-disable no-unused-vars */
+
+//Pagine
 import PageWrapper from './components/PageWrapper';
 import Home from './components/Pages/Home';
+import About from './components/Pages/About';
 
 //Import di react router
 import{
@@ -14,11 +17,17 @@ function App() {
   return (
 	<Router>
 		<PageWrapper>
-			{/*Path: della pagina, arbitrario
+			{/*Exact: serve per quando c'è solo lo slash nel path
+			   Path: della pagina, arbitrario
 			   Component: sostituisce la chiamata a Home.js nel wrapper di <Home/>*/}
 			<Route
+				exact = {true}
 				path = "/" 
 				component = {Home}
+			/>
+			<Route
+				path = "/about" 
+				component = {About}
 			/>
 		</PageWrapper>
 	</Router>
--- sitobootstrap/src/components/Pages/Home.js
+++ sitobootstrap/src/components/Pages/Home.js
@@ -1,19 +1,21 @@
 import React, {Component} from 'react';
+import Header from '../Common/Header';
+import image from '../assets/img/header-bg.jpg'
 
 class Home extends Component{
 
     render(){
         return(
-        //<!-- Masthead-->
-        <div>
-            <header className ="masthead">
-                <div className ="container">
-                    <div className ="masthead-subheading">Welcome To Our Studio!</div>
-                    <div className ="masthead-heading text-uppercase">It's Nice To Meet You</div>
-                    <a className ="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Tell Me More</a>
-                </div>
-            </header>
-        </div>
+            <div>
+                <Header
+                    title = "Prova titolo"
+                    subtitle = "Prova sottotitolo"
+                    buttonText = "Prova testo bottone"
+                    link = "/services"
+                    showButton = {true}
+                    image = {image}
+                />
+            </div>
         );
     }
 }
